cbdc2ab1455781be54477291fdb7904466bea45d
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-21 13:57:31 -0700

    calling names

--- main.js
+++ main.js
@@ -1,74 +1,76 @@
 "use strict";
 
-const characters = [{character: "Rikki", turnPosition: null }, {character: "Cleo", turnPosition: null}, {character: "Emma", turnPosition: null}, {character: "Bella", turnPosition: null}];
+//const characters = [{character: "Rikki", turnPosition: null }, {character: "Cleo", turnPosition: null}, {character: "Emma", turnPosition: null}, {character: "Bella", turnPosition: null}];
 
-let turnOrder = []
+//let turnOrder = []
 
 // GENERATE NUMBER
-function getRandomInteger() {
-  return Math.round(Math.random() * 86)
-}
+//function getRandomInteger() {
+  //return Math.round(Math.random() * characters.length)
+//}
 
 // SET NEXT PLAYER USING MODULO OF RANDOM NUMBER % NUMBER OF PLAYERS
-let nextPlayer = getRandomInteger() % players.length;
+//let nextCharacter = getRandomInteger() % characters.length;
 
 
 // MAKE SURE THE PLAYER ISN'T ALREADY ADDED TO THE LIST OF PLAYERS
-function validatePlayer(currentPlayer, listOfPlayers) {
-  if (!listOfPlayers.includes(currentPlayer)) {
-    return true
-  }
-}
+//function validatePlayer(currentCharacter, listOfCharacters) {
+  //if (!listOfCharacters.includes(currentCharacter)) {
+    //return true
+  //}
+//}
 
 // GENERATE A NEW TURN ORDER
-function setTurnOrder() {
-  turnOrder = []
-  while(turnOrder.length < players.length) {
-    let nextPlayer = getRandomInteger() % players.length;
-    if (validatePlayer(nextPlayer, turnOrder)) {
-      players[nextPlayer].turnPosition = turnOrder.length;
-    turnOrder.push(nextPlayer)
-    }
-  }
-}
+//function setTurnOrder() {
+  //turnOrder = []
+  //while(turnOrder.length < characters.length) {
+    //let nextPlayer = getRandomInteger() % characters.length;
+    //if (validatePlayer(nextPlayer, turnOrder)) {
+     // players[nextPlayer].turnPosition = turnOrder.length;
+    //turnOrder.push(nextPlayer)
+   // }
+  // }
+// }
 
 // RENDER TO DOM LOGIC BELOW
 
 // DOM ELEMENTS TO PASS INFO TO
-const playerList = document.querySelector("#players-list")
-const btn = document.querySelector("#random-player-btn")
-const turnOrderList = document.querySelector("#player-turn-order")
+//const playerList = document.querySelector("#players-list")
+//const btn = document.querySelector("#random-player-btn")
+//const turnOrderList = document.querySelector("#player-turn-order")
 
 
 // create a player list item element
-const createPlayerCard = (player) => {
-  return `<li>Name: ${player.name} </li>`
-}
+//const createPlayerCard = (player) => {
+  //return `<li>Name: ${player.name} </li>`
+//}
 
 // ADD PLAYER CARDS TO THE LIST
-const addPlayerCards = (players) => {
+//const addPlayerCards = (players) => {
   // list through the players
-  players.forEach(player => {
+  //players.forEach(player => {
     // for each player add onne to the unorder list using the create player card function
-    playerList.innerHTML += createPlayerCard(player)
-  })
-}
+    //playerList.innerHTML += createPlayerCard(player)
+  //})
+//}
 
 // set up the original list 
-addPlayerCards(players)
+//addPlayerCards(players)
 
 
 // Render the turn order to the dom
-function renderTurnOrder () {
+//function renderTurnOrder () {
   // set the turn order
-  setTurnOrder()
+  //setTurnOrder()
   // sort the players based on their turn order
-  let sortedPlayers = players.sort((a,b) => a.turnPosition - b.turnPosition)
-  turnOrderList.innerHTML = '';
+  //let sortedPlayers = players.sort((a,b) => a.turnPosition - b.turnPosition)
+  //turnOrderList.innerHTML = '';
   // add the sorted list to the dom
-  sortedPlayers.forEach(player => {
-    turnOrderList.innerHTML += createPlayerCard(player)
-  })
-}
+  //sortedPlayers.forEach(player => {
+    //turnOrderList.innerHTML += createPlayerCard(player)
+  //})
+//}
 // Add event listener so it's clickable
-btn.addEventListener("click", renderTurnOrder)
+//btn.addEventListener("click", renderTurnOrder)
+
+let mermaid = [{mermaid: "Rikki", turnPosition: null }, {mermaid: "Emma", turnPosition: null }, {person: "Cleo", turnPosition: null }, {person: "Bella", turnPosition: null },]
