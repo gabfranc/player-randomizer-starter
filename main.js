d2cc3965696a15eaa170d88d8eb333c34649c83b
Gabrielle Ann Franco <156222595+gabfranc@users.noreply.github.com>
2024-02-21 23:56:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,74 +1,64 @@
 "use strict";
 
-const players = [{name: "newt", turnPosition: null }, {name: "ripley", turnPosition: null}, {name: "apone", turnPosition: null}, {name: "hicks", turnPosition: null}];
 
-let turnOrder = []
+//Done: get players
+const namePlayers = [{name: 'rikki', turnPosition: null},{name: 'cleo', turnPosition: null},{name: 'emma', turnPosition: null},{name: 'bella', turnPosition: null}];
+//console.log(players);
 
-// GENERATE NUMBER
+//TODO: set turn order
+let turnOrder = [];
+
+//TODO: generate a random number
 function getRandomInteger() {
-  return Math.round(Math.random() * 86)
+  return Math.round(Math.random() * namePlayers.length);
 }
+//TODO: get the remainder from that number
+let nextPlayer = getRandomInteger() % namePlayers.length;
+console.log(nextPlayer);
 
-// SET NEXT PLAYER USING MODULO OF RANDOM NUMBER % NUMBER OF PLAYERS
-let nextPlayer = getRandomInteger() % players.length;
+//TODO: Make sure no repeat players are added
 
-
-// MAKE SURE THE PLAYER ISN'T ALREADY ADDED TO THE LIST OF PLAYERS
-function validatePlayer(currentPlayer, listOfPlayers) {
-  if (!listOfPlayers.includes(currentPlayer)) {
+function newPlayerName(currentPlayerName,listPlayerName) {
+  if (!listPlayerName.includes(currentPlayerName)) {
     return true
-  }
-}
+  };
+};
+//TODO: add players to the turn order
+function setOrderList() {
+  while(turnOrder.length < players.length){
+    let nextPlayer = getRandomInteger() % namePlayers.length;
+    namePlayers[nextPlayer].turnPosition = turnOrder.length;
+    turnOrder.push(nextPlayer);
+  };
+};
 
-// GENERATE A NEW TURN ORDER
-function setTurnOrder() {
-  turnOrder = []
-  while(turnOrder.length < players.length) {
-    let nextPlayer = getRandomInteger() % players.length;
-    if (validatePlayer(nextPlayer, turnOrder)) {
-      players[nextPlayer].turnPosition = turnOrder.length;
-    turnOrder.push(nextPlayer)
-    }
-  }
-}
-
-// RENDER TO DOM LOGIC BELOW
-
-// DOM ELEMENTS TO PASS INFO TO
-const playerList = document.querySelector("#players-list")
-const btn = document.querySelector("#random-player-btn")
-const turnOrderList = document.querySelector("#player-turn-order")
+const nameList = document.querySelector(".player-names");
+const button = document.querySelector(".randomize-player");
+const orderList = document.querySelector(".layer-orderList");
 
 
-// create a player list item element
+
 const createPlayerCard = (player) => {
-  return `<li>Name: ${player.name} </li>`
+  return '<li> ${player.name}</li>'
 }
 
-// ADD PLAYER CARDS TO THE LIST
-const addPlayerCards = (players) => {
-  // list through the players
-  players.forEach(player => {
-    // for each player add onne to the unorder list using the create player card function
-    playerList.innerHTML += createPlayerCard(player)
+const addPlayerCards = (playersList) => {
+  //list of player names
+    nextPlayer.forEach(player => {
+    //for each player add one to the unorder list using the create player card function
+    nameList.innerHTML += createPlayerCard(player);
+  });
+};
+addPlayerCards();
+
+function namesRendered() {
+  setOrderList()
+  let playerSorted = playerNames.playerSorted((a, b) => a.turnPosition -b.turnPosition);
+  orderList.innerHTML = '';
+  playerSorted.forEach(player => {
+  orderList.textContent += createPlayerCard(player);
   })
+
 }
 
-// set up the original list 
-addPlayerCards(players)
-
-
-// Render the turn order to the dom
-function renderTurnOrder () {
-  // set the turn order
-  setTurnOrder()
-  // sort the players based on their turn order
-  let sortedPlayers = players.sort((a,b) => a.turnPosition - b.turnPosition)
-  turnOrderList.innerHTML = '';
-  // add the sorted list to the dom
-  sortedPlayers.forEach(player => {
-    turnOrderList.innerHTML += createPlayerCard(player)
-  })
-}
-// Add event listener so it's clickable
-btn.addEventListener("click", renderTurnOrder)
+button.addEventListener("click", namesRendered);
